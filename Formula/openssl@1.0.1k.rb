023d6bd58d2a9cea740da84157a859ad201b9829
Andrew LaChasse <buddilla@buddillalappy.local>
2020-01-23 19:41:40 -0800

    Update openssl@1.0.1k.rb

--- Formula/openssl@1.0.1k.rb
+++ Formula/openssl@1.0.1k.rb
@@ -75,7 +75,7 @@ test do
 
     # Check OpenSSL itself functions as expected.
     (testpath/"testfile.txt").write("This is a test file")
-    expected_checksum = "91b7b0b1e27bfbf7bc646946f35fa972c47c2d32"
+    expected_checksum = "BE4D6F7C9943DD408F8B5838F972D98E55EF6100"
     system "#{bin}/openssl", "dgst", "-sha1", "-out", "checksum.txt", "testfile.txt"
     open("checksum.txt") do |f|
       checksum = f.read(100).split("=").last.strip
