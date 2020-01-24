68b63ea2a42a016060fad2655f491d2dd32eff72
Andrew LaChasse <buddilla@buddillalappy.local>
2020-01-23 19:57:54 -0800

    Update openssl@1.0.1k.rb

--- Formula/openssl@1.0.1k.rb
+++ Formula/openssl@1.0.1k.rb
@@ -1,14 +1,14 @@
 class OpensslAT101k < Formula
   homepage "https://openssl.org"
-  url "https://github.com/2a5A1Ghu1/homebrew-CryptoTap/raw/master/prerequisites/openssl/openssl-1.0.1k.tar.gz"
-  mirror "https://www.openssl.org/source/openssl-1.0.1k.tar.gz"
-  sha256 "8f9faeaebad088e772f4ef5e38252d472be4d878c6b3a2718c10a4fcebe7a41c"
+  url "https://www.openssl.org/source/openssl-1.0.1l.tar.gz"
+  mirror "https://raw.githubusercontent.com/DomT4/LibreMirror/master/OpenSSL/openssl-1.0.1l.tar.gz"
+  sha256 "b2cf4d48fe5d49f240c61c9e624193a6f232b5ed0baf010681e725963c40d1d4"
 
-  #bottle do
-  #  sha1 "3d0e5529a124be70266dd2a2074f4f84db38bb19" => :yosemite
-  #  sha1 "449b81391bd9718b1ed7a37678c686b712669f38" => :mavericks
-  #  sha1 "8f1b30f6352486726b8420e80cceeecd49a61f82" => :mountain_lion
-  #end
+  bottle do
+    sha1 "1d804c229e7a49cf98c1211dd2524c1b258a0388" => :yosemite
+    sha1 "c5087d42713738542d32eb50dbfcdf5143cb5ad3" => :mavericks
+    sha1 "4a72fe4a9d5d5914822d82f42df2c0f3392bfa6e" => :mountain_lion
+  end
 
   option :universal
   option "without-check", "Skip build-time tests (not recommended)"
@@ -18,30 +18,82 @@ class OpensslAT101k < Formula
   keg_only :provided_by_osx,
     "Apple has deprecated use of OpenSSL in favor of its own TLS and crypto libraries"
 
-  def install
-    # OpenSSL will prefer the PERL environment variable if set over $PATH
-    # which can cause some odd edge cases & isn't intended. Unset for safety,
-    # along with perl modules in PERL5LIB.
-    ENV.delete("PERL")
-    ENV.delete("PERL5LIB")
+  def arch_args
+    {
+      :x86_64 => %w[darwin64-x86_64-cc enable-ec_nistp_64_gcc_128],
+      :i386   => %w[darwin-i386-cc],
+    }
+  end
 
-    ENV.deparallelize
-    args = %W[
+  def configure_args; %W[
       --prefix=#{prefix}
       --openssldir=#{openssldir}
       no-ssl2
-      no-ssl3
-      no-zlib
+      zlib-dynamic
       shared
       enable-cms
-      darwin64-x86_64-cc
-      enable-ec_nistp_64_gcc_128
     ]
-    system "perl", "./Configure", *args
-    system "make", "depend"
-    system "make"
-    system "make", "test"
+  end
+
+  def install
+    if build.universal?
+      ENV.permit_arch_flags
+      archs = Hardware::CPU.universal_archs
+    elsif MacOS.prefer_64_bit?
+      archs = [Hardware::CPU.arch_64_bit]
+    else
+      archs = [Hardware::CPU.arch_32_bit]
+    end
+
+    dirs = []
+
+    archs.each do |arch|
+      if build.universal?
+        dir = "build-#{arch}"
+        dirs << dir
+        mkdir dir
+        mkdir "#{dir}/engines"
+        system "make", "clean"
+      end
+
+      ENV.deparallelize
+      system "perl", "./Configure", *(configure_args + arch_args[arch])
+      system "make", "depend"
+      system "make"
+
+      if (MacOS.prefer_64_bit? || arch == MacOS.preferred_arch) && build.with?("check")
+        system "make", "test"
+      end
+
+      if build.universal?
+        cp Dir["*.?.?.?.dylib", "*.a", "apps/openssl"], dir
+        cp Dir["engines/**/*.dylib"], "#{dir}/engines"
+      end
+    end
+
     system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl"
+
+    if build.universal?
+      %w[libcrypto libssl].each do |libname|
+        system "lipo", "-create", "#{dirs.first}/#{libname}.1.0.0.dylib",
+                                  "#{dirs.last}/#{libname}.1.0.0.dylib",
+                       "-output", "#{lib}/#{libname}.1.0.0.dylib"
+        system "lipo", "-create", "#{dirs.first}/#{libname}.a",
+                                  "#{dirs.last}/#{libname}.a",
+                       "-output", "#{lib}/#{libname}.a"
+      end
+
+      Dir.glob("#{dirs.first}/engines/*.dylib") do |engine|
+        libname = File.basename(engine)
+        system "lipo", "-create", "#{dirs.first}/engines/#{libname}",
+                                  "#{dirs.last}/engines/#{libname}",
+                       "-output", "#{lib}/engines/#{libname}"
+      end
+
+      system "lipo", "-create", "#{dirs.first}/openssl",
+                                "#{dirs.last}/openssl",
+                     "-output", "#{bin}/openssl"
+    end
   end
 
   def openssldir
@@ -68,14 +120,14 @@ class OpensslAT101k < Formula
     EOS
   end
 
-test do
+  test do
     # Make sure the necessary .cnf file exists, otherwise OpenSSL gets moody.
     assert (HOMEBREW_PREFIX/"etc/openssl/openssl.cnf").exist?,
             "OpenSSL requires the .cnf file for some functionality"
 
     # Check OpenSSL itself functions as expected.
     (testpath/"testfile.txt").write("This is a test file")
-    expected_checksum = "BE4D6F7C9943DD408F8B5838F972D98E55EF6100"
+    expected_checksum = "91b7b0b1e27bfbf7bc646946f35fa972c47c2d32"
     system "#{bin}/openssl", "dgst", "-sha1", "-out", "checksum.txt", "testfile.txt"
     open("checksum.txt") do |f|
       checksum = f.read(100).split("=").last.strip
@@ -83,3 +135,4 @@ test do
     end
   end
 end
+
